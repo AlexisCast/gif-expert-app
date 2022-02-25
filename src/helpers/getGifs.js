9985ca9ddf04a8eb5a885de94b6e4dbfe085fb33
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-02-24 18:19:34 -0600

    index.html modified fixed due to http error

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -5,7 +5,7 @@
 export const GetGifs = async (category) => {
    // const test='naruto';
    const url =
-      `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=oBJaWvD8oC03bP7ULhrHGNOkar6Jns6Y`;
+      `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=oBJaWvD8oC03bP7ULhrHGNOkar6Jns6Y`;
    const resp = await fetch(url);
    const { data } = await resp.json();
    const gifs = data.map((img) => {
